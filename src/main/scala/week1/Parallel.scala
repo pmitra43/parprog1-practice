b336e6130505787aadd20b1556cc3b20d048b645
Priyam Mitra <mpriyam@thoughtworks.com>
2017-06-15 14:38:13 +0530

    Creating trait Task, and implementing Parallel using Task

--- src/main/scala/week1/Parallel.scala
+++ src/main/scala/week1/Parallel.scala
@@ -1,5 +1,16 @@
 package week1
 
+
+trait Task[A] {
+  def join: A
+}
+
 object Parallel {
-  def parallel[A, B](taskA: => A, taskB: => B): (A, B) = { ??? }
+  def task[A](c: => A): Task[A] = { ??? }
+
+  def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
+    val tB: Task[B] = task {taskB}
+    val tA: A = taskA
+    (tA, tB.join)
+  }
 }
